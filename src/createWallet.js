eb2211f74c1ecea14a90ff6b48a77175e4f7f9f3
vanessavpg <vanessa.pg026@gmail.com>
2024-01-19 16:53:33 -0300

    wallet

---
+++ src/createWallet.js
@@ -0,0 +1,44 @@
+// importando as dependências
+
+
+const bip32 = require('bip32');
+
+const bip39 = require('bip39');
+const bitcoin = require('bitcoinjs-lib');
+
+// definir a rede
+
+// bitcoin - rede principal: mainnet
+// bitcoin - rede de teste: testnet
+
+const network = bitcoin.networks.testnet;
+
+// derivação de carteiras HD
+
+const path = `m/49'/1'/0'/0`;
+
+
+// criando o mnemônico para a seed
+let mnemonic = bip39.generateMnemonic();
+const seed = bip39.mnemonicToSeedSync(mnemonic);
+
+// criando a raiz da carteira HD
+let root = bip32.fromSeed(seed, network);
+
+
+//criando uma conta - par de chaves privadas e chaves públicas
+let account = root.derivePath(path);
+let node = account.derive(0).derive(0);
+
+
+let btcAddress = bitcoin.payments.p2pkh({
+  pubkey: node.publicKey,
+  network: network
+}).address;
+
+
+
+console.log("Carteira gerada");
+console.log("Endereço: " + btcAddress);
+console.log("Chave privada: " + node.toWIF());
+console.log("Seed", mnemonic)
